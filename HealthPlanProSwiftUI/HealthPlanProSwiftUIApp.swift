e916997a46604ded76a90180bf90d8047d54b24e
Ngoc Ha <ngocha38450510@gmail.com>
2025-07-18 15:10:34 +0700

    Initial Commit

---
+++ HealthPlanProSwiftUI/HealthPlanProSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPlanProSwiftUIApp.swift
+//  HealthPlanProSwiftUI
+//
+//  Created by iKame Elite Fresher 2025 on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthPlanProSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
