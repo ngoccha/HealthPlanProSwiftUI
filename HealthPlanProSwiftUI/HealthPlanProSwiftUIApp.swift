544ecea54cc65e00ddafe27af51cde536837d5dd
Ngoc Ha <ngocha38450510@gmail.com>
2025-07-28 14:27:22 +0700

    fix some bugs

--- HealthPlanProSwiftUI/HealthPlanProSwiftUIApp.swift
+++ HealthPlanProSwiftUI/HealthPlanProSwiftUIApp.swift
@@ -11,38 +11,34 @@ import SwiftUI
 struct HealthPlanProSwiftUIApp: App {
     @StateObject var settingManager = SettingManager()
     @AppStorage("done") var isDoneIntro: Bool = false
-
+    
     var body: some Scene {
         WindowGroup {
-            if isDoneIntro {
-                NavigationStack(path: $settingManager.settingPath) {
-                    TabHomeView()
-                        .onAppear {
-                            settingManager.selectedTab = .log
-                        }
-                        .environmentObject(settingManager)
-                }
-            } else {
-                NavigationStack(path: $settingManager.settingPath) {
-                    FirstPageView()
-                        .environmentObject(settingManager)
-                        .navigationDestination(for: Destination.self) { destination in
-                            switch destination {
-                            case .profile:
-                                ProfileView()
-                                    .environmentObject(settingManager)
-                            case .information:
-                                InformationView()
-                                    .environmentObject(settingManager)
-                            case .setting:
-                                //                            SettingView().environmentObject(settingManager)
-                                TabHomeView()
-                                    .onAppear {
-                                        settingManager.selectedTab = .setting
-                                    }
-                                    .environmentObject(settingManager)
+            NavigationStack(path: $settingManager.settingPath) {
+                Group {
+                    if isDoneIntro {
+                        TabHomeView()
+                            .onAppear {
+                                settingManager.selectedTab = .log
                             }
-                        }
+                    } else {
+                        FirstPageView()
+                    }
+                }
+                .environmentObject(settingManager)
+                .navigationDestination(for: Destination.self) { destination in
+                    switch destination {
+                    case .profile:
+                        ProfileView().environmentObject(settingManager)
+                    case .information:
+                        InformationView().environmentObject(settingManager)
+                    case .setting:
+                        TabHomeView()
+                            .onAppear {
+                                settingManager.selectedTab = .setting
+                            }
+                            .environmentObject(settingManager)
+                    }
                 }
             }
         }
